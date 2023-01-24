31d4a06df0f4502382b6440f8ff13cd114e42ccc
babaicode <andreymarachevskiy@gmail.com>
2023-01-24 22:03:23 +0100

    ``

---
+++ src/NewNoteInput.tsx
@@ -0,0 +1,24 @@
+import React, { ChangeEvent } from "react";
+
+interface NewNoteInputProps {
+         addNote(note: string): void;
+
+}
+export const NewNoteInput: React.FC<NewNoteInputProps> = ({ addNote }) => {
+         const [note, setNote] = React.useState("");
+
+         const updateNote = (event: ChangeEvent<HTMLInputElement>) => {
+                  setNote(event.target.value)
+         }
+
+         const onAddNoteClick = () => {
+                  addNote(note)
+                  setNote("")
+         }
+         return (
+                  <div>
+                           <input onChange={updateNote} value={note} type="text" name="note" placeholder="note" />
+                           <button onClick={onAddNoteClick}>add note</button>
+                  </div>
+         )
+}
